6a7047033e49cd06bf7db5daeab160d84e85fb5c
programmermosharof <mosharof.dev@gmail.com>
2026-02-05 15:41:25 +0600

    task 1 is complete

---
+++ task1.js
@@ -0,0 +1,10 @@
+
+// ### Task-1: 
+// Write a function to convert temperature from Celsius to Fahrenheit.
+
+function celsiusToFahrenheit(Celsius){
+    const fahrenheit = (Celsius * 9 / 5) + 30 ;
+    return fahrenheit;
+
+}
+console.log(celsiusToFahrenheit(3));
